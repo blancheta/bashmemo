2b05bc50a3fa13d238fbac467690d580a8b76298
Alexandre Blanchet <aldazar@alexandresmbp2.home>
2022-08-24 22:33:26 +0100

    feat: add options

--- bashmemo/main.py
+++ bashmemo/main.py
@@ -1,5 +1,14 @@
-def run():
+import click
+
+@click.command()
+@click.option("--autodiscover", default=1, help="Autodiscover most used commands from your bash history to bookmark")
+@click.option('--bookmark', prompt='command',
+              help='The command to bookmark')
+def run(autodiscover, bookmark):
     print("Welcome to BashMemo!")
+
+    if None not in (autodiscover, bookmark):
+        raise click.ClickException('filename argument and option string are mutually exclusive!')
 
 
 if __name__ == '__main__':
