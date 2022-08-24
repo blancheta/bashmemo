7b4c98a17f9de7be64594ac31bffb2645859f5a9
Alexandre Blanchet <aldazar@alexandresmbp2.home>
2022-08-24 22:18:33 +0100

    feat: add setup.py and pyproject.toml

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-if __name__ == '__main__':
+def run():
     print("Welcome to BashMemo!")
+
+
+if __name__ == '__main__':
+    run()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
---
+++ setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fhand:
+    long_description = fhand.read()
+
+setuptools.setup(
+    name="bashmemo",
+    version="0.0.1",
+    author="Liu Zheng",
+    author_email="zheng@example.com",
+    description=(
+        "One command once. BashMemo memorises commands for you."
+        "Access it from anywhere logging with your github account"),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/blancheta/bashmemo",
+    project_urls={
+        "Bug Tracker": "https://github.com/blancheta/bashmemo/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=["requests"],
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+    entry_points={
+        "console_scripts": [
+            "bm = bashmemo.main:",
+        ]
+    }
+)
